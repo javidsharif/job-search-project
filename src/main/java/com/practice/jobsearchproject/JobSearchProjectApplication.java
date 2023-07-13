97eb1dd821ce171c1c845799d7f0e47d66ef509d
Djavid001 <cavid.sarifaliyev@gmail.com>
2023-07-13 20:35:51 +0400

    first commit

---
+++ src/main/java/com/practice/jobsearchproject/JobSearchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.jobsearchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/jobsearchproject/JobSearchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jobsearchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
